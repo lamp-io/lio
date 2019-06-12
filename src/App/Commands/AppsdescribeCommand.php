7217ab99edbc46ec76eaf5a19194484fa1882e53
jbartus <jim@bartus.com>
2019-06-12 00:47:54 +0000

    babys first cli

---
+++ src/App/Commands/AppsdescribeCommand.php
@@ -0,0 +1,40 @@
+<?php
+namespace Console\App\Commands;
+ 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+ 
+class AppsdescribeCommand extends Command
+{
+    protected static $defaultName = 'apps:describe';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('gets the apps you specify')
+            ->setHelp('try rebooting')
+            ->addArgument('app_name', InputArgument::REQUIRED, 'which app would you like to describe?')
+        ;
+    }
+ 
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $token = file_get_contents('/root/.config/lamp.io/token');
+        $curl_handle = curl_init();
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_handle, CURLOPT_URL, "https://api.lamp.io/apps/" . $input->getArgument('app_name'));
+        curl_setopt($curl_handle, CURLOPT_HTTPHEADER, array(
+            "Authorization: Bearer $token",
+            'Content-type: application/vnd.api+json',
+            'Accept: application/vnd.api+json'
+        ));
+
+        $output->writeln(curl_exec($curl_handle));
+
+        curl_close($curl_handle);
+    }
+}
+
