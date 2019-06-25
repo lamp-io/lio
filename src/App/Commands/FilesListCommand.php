ca03c4c649d39da6ddad3c1360a584a07b82bf04
Kirill <sooperkiras@gmail.com>
2019-06-25 23:29:30 +0300

    fix receiving file path, annotations

--- src/App/Commands/FilesListCommand.php
+++ src/App/Commands/FilesListCommand.php
@@ -12,64 +12,86 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class FilesListCommand extends Command
 {
-    const API_ENDPOINT = 'https://api.lamp.io/apps/{app_name}/files';
+	const API_ENDPOINT = 'https://api.lamp.io/apps/{app_name}/files';
 
-    const RESPONSE_FORMAT_TYPES = [
-        'json' => [
-            'AcceptHeader' => 'Accept: application/vnd.api+json',
-        ],
-        'gzip' => [
-            'AcceptHeader' => 'Accept: application/x-gzip',
-        ],
-        'zip'  => [
-            'AcceptHeader' => 'Accept: application/zip',
-        ],
-    ];
+	const RESPONSE_FORMAT_TYPES = [
+		'json' => [
+			'AcceptHeader' => 'Accept: application/vnd.api+json',
+		],
+		'gzip' => [
+			'AcceptHeader' => 'Accept: application/x-gzip',
+		],
+		'zip'  => [
+			'AcceptHeader' => 'Accept: application/zip',
+		],
+	];
 
-    protected static $defaultName = 'files:list';
+	protected static $defaultName = 'files:list';
 
-    protected function configure()
-    {
-        $this->setDescription('Return files from the root of an app')
-            ->setHelp('try rebooting')
-            ->addArgument('app_id', InputArgument::REQUIRED, 'From which app_id need to get fiels?')
-            ->addOption('path', 'p', InputOption::VALUE_REQUIRED, 'Path where will be stored received data list. Dont forget that this path should has write permissions', '')
-            ->addOption('json', 'j', InputOption::VALUE_NONE, 'Set this flag, if you want response as a json')
-            ->addOption('gzip', 'g', InputOption::VALUE_NONE, 'Set this flag, if you want response as a gzip archive')
-            ->addOption('zip', 'z', InputOption::VALUE_NONE, 'Set this flag, if you want response as a zip archive.');
-    }
+	/**
+	 *
+	 */
+	protected function configure()
+	{
+		$this->setDescription('Return files from the root of an app')
+			->setHelp('try rebooting')
+			->addArgument('app_id', InputArgument::REQUIRED, 'From which app_id need to get fiels?')
+			->addOption('json', 'j', InputOption::VALUE_NONE, 'Set this flag, if you want response as a json')
+			->addOption('gzip', 'g', InputOption::VALUE_NONE, 'Set this flag, if you want response as a gzip archive')
+			->addOption('zip', 'z', InputOption::VALUE_NONE, 'Set this flag, if you want response as a zip archive.');
+	}
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        parent::execute($input, $output);
-        $format = $this->getResponseFormat($input);
-        try {
-            $this->httpHelper->getClient()->request(
-                'GET',
-                str_replace('{app_name}', $input->getArgument('app_id'), self::API_ENDPOINT),
-                [
-                    'headers' => array_merge($this->httpHelper->getHeaders(), ['Accept' => self::RESPONSE_FORMAT_TYPES[$format]['AcceptHeader']]),
-                    'sink' => fopen($input->getOption('path') . 'lamp.' . $format, 'w'),
-                ]
-            );
-            $output->writeln('File received, ' . $input->getOption('path') . 'lamp.' . $format);
-        } catch (GuzzleException $guzzleException) {
-            $output->writeln($guzzleException->getMessage());
-            die();
-        }
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int|null|void
+	 * @throws \Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+		$format = $this->getResponseFormat($input);
+		try {
+			$this->httpHelper->getClient()->request(
+				'GET',
+				str_replace('{app_name}', $input->getArgument('app_id'), self::API_ENDPOINT),
+				[
+					'headers' => array_merge($this->httpHelper->getHeaders(), ['Accept' => self::RESPONSE_FORMAT_TYPES[$format]['AcceptHeader']]),
+					'sink'    => fopen($this->getPath('lamp') . '.' . $format, 'w'),
+				]
+			);
+			$output->writeln('File received, ' . $this->getPath('lamp') . '.' . $format);
+		} catch (GuzzleException $guzzleException) {
+			$output->writeln($guzzleException->getMessage());
+			die();
+		}
 
-    }
+	}
 
-    protected function getResponseFormat(InputInterface $input): string
-    {
-        $format = 'json';
-        foreach ($input->getOptions() as $key => $option) {
-            if (array_key_exists($key, self::RESPONSE_FORMAT_TYPES) && !empty($option)) {
-                $format = $key;
-            }
-        }
-        return $format;
-    }
+	/**
+	 * @param string $fileName
+	 * @return string
+	 */
+	protected function getPath(string $fileName): string
+	{
+		return getenv('HOME') . getenv("HOMEDRIVE") . getenv("HOMEPATH") . DIRECTORY_SEPARATOR . '.config' . DIRECTORY_SEPARATOR . '.lamp.io/' . $fileName;
+	}
+
+
+	/**
+	 * @param InputInterface $input
+	 * @return string
+	 */
+	protected function getResponseFormat(InputInterface $input): string
+	{
+		$format = 'json';
+		foreach ($input->getOptions() as $key => $option) {
+			if (array_key_exists($key, self::RESPONSE_FORMAT_TYPES) && !empty($option)) {
+				$format = $key;
+			}
+		}
+		return $format;
+	}
 
 
 }
