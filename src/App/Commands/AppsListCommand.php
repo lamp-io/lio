5babaa27b47b9066d1d06c6fb493dc29efe9c64f
Kirill <sooperkiras@gmail.com>
2019-07-13 23:27:59 +0300

    raw json output apps:list

--- src/App/Commands/AppsListCommand.php
+++ src/App/Commands/AppsListCommand.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Art4\JsonApiClient\Exception\ValidationException;
 use Art4\JsonApiClient\Helper\Parser;
@@ -21,7 +22,8 @@ class AppsListCommand extends Command
 	protected function configure()
 	{
 		$this->setDescription('gets the set of apps from the org associated with your token')
-			->setHelp('try rebooting');
+			->setHelp('try rebooting')
+			->addOption('json', 'j', InputOption::VALUE_NONE, 'Output as a raw json');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
@@ -40,26 +42,35 @@ class AppsListCommand extends Command
 		}
 
 		try {
-			/** @var Document $document */
-			$document = Parser::parseResponseString($response->getBody()->getContents());
-			$serializer = new ArraySerializer(['recursive' => true]);
-			$table = new Table($output);
-			$table->setHeaderTitle('Apps');
-			$table->setHeaders(['App id', 'App description']);
-			$apps = $serializer->serialize($document);
-			foreach ($apps['data'] as $key => $app) {
-				$table->addRow([
-					$app['id'],
-					$app['attributes']['description'],
-				]);
-				if ($key != count($apps['data']) - 1) {
-					$table->addRow(new TableSeparator());
-				}
+			if (!empty($input->getOption('json'))) {
+				$output->writeln($response->getBody()->getContents());
+			} else {
+				/** @var Document $document */
+				$document = Parser::parseResponseString($response->getBody()->getContents());
+				$table = $this->getOutputAsTable($document, new Table($output));
+				$table->render();
 			}
-			$table->render();
 		} catch (ValidationException $e) {
 			$output->writeln($e->getMessage());
 		}
+	}
 
+	protected function getOutputAsTable(Document $document, Table $table): Table
+	{
+		$table->setHeaderTitle('Apps');
+		$table->setHeaders(['App id', 'App description']);
+		$serializer = new ArraySerializer(['recursive' => true]);
+		$apps = $serializer->serialize($document);
+		foreach ($apps['data'] as $key => $app) {
+			$table->addRow([
+				$app['id'],
+				$app['attributes']['description'],
+			]);
+			if ($key != count($apps['data']) - 1) {
+				$table->addRow(new TableSeparator());
+			}
+		}
+
+		return $table;
 	}
 }
