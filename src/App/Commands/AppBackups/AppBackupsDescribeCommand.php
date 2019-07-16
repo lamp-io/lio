b010a9aaa5ebeef2130fa0a3f7ad6b1e8c0889f6
Kirill <sooperkiras@gmail.com>
2019-07-17 01:20:09 +0300

    add app_backups:describe command

---
+++ src/App/Commands/AppBackups/AppBackupsDescribeCommand.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Console\App\Commands\AppBackups;
+
+use Console\App\Commands\Command;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Art4\JsonApiClient\V1\Document;
+use Symfony\Component\Console\Helper\Table;
+use Art4\JsonApiClient\Helper\Parser;
+
+class AppBackupsDescribeCommand extends Command
+{
+	protected static $defaultName = 'app_backups:describe';
+
+	const API_ENDPOINT = 'https://api.lamp.io/app_backups/%s';
+
+	/**
+	 *
+	 */
+	protected function configure()
+	{
+		$this->setDescription('Return an app backup')
+			->setHelp('https://www.lamp.io/api#/app_backups/appBackupsShow')
+			->addArgument('app_backup_id', InputArgument::REQUIRED, 'The ID of the app backup')
+			->addOption('json', 'j', InputOption::VALUE_NONE, 'Output as a raw json');
+	}
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int|void|null
+	 * @throws \Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+
+		try {
+			$response = $this->httpHelper->getClient()->request(
+				'GET',
+				sprintf(
+					self::API_ENDPOINT,
+					$input->getArgument('app_backup_id')
+				),
+				[
+					'headers' => $this->httpHelper->getHeaders(),
+				]
+			);
+			if (!empty($input->getOption('json'))) {
+				$output->writeln($response->getBody()->getContents());
+			} else {
+				/** @var Document $document */
+				$document = Parser::parseResponseString($response->getBody()->getContents());
+				$table = $this->getOutputAsTable($document, new Table($output));
+				$table->render();
+			}
+		} catch (GuzzleException $guzzleException) {
+			$output->writeln('<error>' . $guzzleException->getMessage() . '</error>');
+			exit(1);
+		}
+	}
+
+	/**
+	 * @param Document $document
+	 * @param Table $table
+	 * @return Table
+	 */
+	protected function getOutputAsTable(Document $document, Table $table): Table
+	{
+		$table->setHeaderTitle('App Backup ' . $document->get('data.id'));
+		$table->setHeaders(['App id', 'Complete', 'Created at', 'Organization id', 'Status', 'Updated at']);
+		$table->addRow([
+			$document->get('data.attributes.app_id'),
+			$document->get('data.attributes.complete'),
+			$document->get('data.attributes.created_at'),
+			$document->get('data.attributes.organization_id'),
+			$document->get('data.attributes.status'),
+			$document->get('data.attributes.updated_at'),
+		]);
+		return $table;
+	}
+}
