aa40516c80b37d56f1b98febaf0b4284714ff182
KirillPerepelitsa <Sooperkiras@gmail.com>
2019-07-23 12:34:40 +0300

    fix after merge

--- src/App/Commands/Command.php
+++ src/App/Commands/Command.php
@@ -8,7 +8,6 @@ use GuzzleHttp\ClientInterface;
 use Symfony\Component\Console\Command\Command as BaseCommand;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
