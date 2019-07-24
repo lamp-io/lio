b4c001fd178dbe98bdc2121b0908529e1164393e
Kirill <sooperkiras@gmail.com>
2019-07-25 00:15:05 +0300

    database to databases

--- src/App/Commands/Databases/DatabasesListCommand.php
+++ src/App/Commands/Databases/DatabasesListCommand.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace Console\App\Commands\Database;
+namespace Console\App\Commands\Databases;
 
 
 use Art4\JsonApiClient\Helper\Parser;
@@ -10,17 +10,22 @@ use Art4\JsonApiClient\V1\Document;
 use Console\App\Commands\Command;
 use Exception;
 use GuzzleHttp\Exception\GuzzleException;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use InvalidArgumentException;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class DatabaseDescribeCommand extends Command
+class DatabasesListCommand extends Command
 {
-	protected static $defaultName = 'database:describe';
+	protected static $defaultName = 'databases:list';
 
-	const API_ENDPOINT = 'https://api.lamp.io/databases/%s';
+	const API_ENDPOINT = 'https://api.lamp.io/databases%s';
+
+	const OPTIONS_TO_QUERY_KEYS = [
+		'organization_id' => 'filter[organization_id]',
+	];
 
 	/**
 	 *
@@ -28,9 +33,9 @@ class DatabaseDescribeCommand extends Command
 	protected function configure()
 	{
 		parent::configure();
-		$this->setDescription('Returns a database')
-			->setHelp('https://www.lamp.io/api#/databases/databasesShow')
-			->addArgument('database_id', InputArgument::REQUIRED, 'The id of database');
+		$this->setDescription('Returns all allowed databases')
+			->setHelp('https://www.lamp.io/api#/databases/databasesList')
+			->addOption('organization_id', null, InputOption::VALUE_REQUIRED, 'Filter output by organization id value');
 	}
 
 	/**
@@ -45,9 +50,8 @@ class DatabaseDescribeCommand extends Command
 		try {
 			$response = $this->httpHelper->getClient()->request(
 				'GET',
-				sprintf(
-					self::API_ENDPOINT,
-					$input->getArgument('database_id')
+				sprintf(self::API_ENDPOINT,
+					$this->httpHelper->optionsToQuery($input->getOptions(), self::OPTIONS_TO_QUERY_KEYS)
 				),
 				[
 					'headers' => $this->httpHelper->getHeaders(),
@@ -80,18 +84,28 @@ class DatabaseDescribeCommand extends Command
 	{
 		$table->setHeaderTitle('Database');
 		$serializer = new ArraySerializer(['recursive' => true]);
-		$headers = ['Id'];
-		$row = [$document->get('data.0.id')];
-		foreach ($serializer->serialize($document)['data'][0]['attributes'] as $attributeKey => $attribute) {
-			array_push(
-				$row, ($attributeKey != 'my_cnf') ? $attribute : wordwrap($attribute, 30)
-			);
-			array_push($headers, $attributeKey);
+		$serializedDocument = $serializer->serialize($document);
+		$header = ['Id', 'Attributes'];
+		$table->setHeaders($header);
+		foreach ($serializedDocument['data'] as $key => $data) {
+			$row = [$data['id']];
+			$attributeArray = [];
+			foreach ($data['attributes'] as $attributeKey => $attribute) {
+				if ($attributeKey != 'my_cnf') {
+					$attributeArray[] = $attributeKey . ' : ' . wordwrap($attribute, 50);
+				} else {
+					$attributeArray[] = $attributeKey . ' : ' . $attribute;
+				}
+			}
+			$row[] = (implode(PHP_EOL, $attributeArray));
+			$table->addRow($row);
+			if ($key != count($serializedDocument['data']) - 1) {
+				$table->addRow(new TableSeparator());
+			}
+
 		}
-		$table->setHeaders($headers);
-		$table->addRow($row);
+		$table->setColumnWidth(1, 200);
 		return $table;
 
 	}
 }
-
--- src/App/Commands/Databases/DatabasesNewCommand.php
+++ src/App/Commands/Databases/DatabasesNewCommand.php
@@ -1,29 +1,26 @@
 <?php
 
+namespace Console\App\Commands\Databases;
 
-namespace Console\App\Commands\Database;
-
-
-use Art4\JsonApiClient\Helper\Parser;
-use Art4\JsonApiClient\Serializer\ArraySerializer;
-use Art4\JsonApiClient\V1\Document;
 use Console\App\Commands\Command;
 use GuzzleHttp\Exception\GuzzleException;
 use InvalidArgumentException;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Art4\JsonApiClient\V1\Document;
+use Symfony\Component\Console\Helper\Table;
+use Art4\JsonApiClient\Serializer\ArraySerializer;
+use Art4\JsonApiClient\Helper\Parser;
 
-class DatabaseUpdateCommand extends Command
+class DatabasesNewCommand extends Command
 {
-	protected static $defaultName = 'database:update';
+	protected static $defaultName = 'databases:new';
 
-	const API_ENDPOINT = 'https://api.lamp.io/databases/%s';
+	const API_ENDPOINT = 'https://api.lamp.io/databases';
 
 	const EXCLUDE_FROM_OUTPUT = [
-		'my_cnf',
+		'my_cnf'
 	];
 
 	/**
@@ -32,16 +29,15 @@ class DatabaseUpdateCommand extends Command
 	protected function configure()
 	{
 		parent::configure();
-		$this->setDescription('Update a database')
-			->setHelp('https://www.lamp.io/api#/databases/databasesUpdate')
-			->addArgument('database_id', InputArgument::REQUIRED, 'The id of database')
-			->addOption('description', 'd', InputOption::VALUE_REQUIRED, 'Description of your database')
-			->addOption('memory', 'm', InputOption::VALUE_REQUIRED, 'Amount of virtual memory on your database')
-			->addOption('organization_id', null, InputOption::VALUE_REQUIRED, 'Name of your organization')
-			->addOption('my_cnf', null, InputOption::VALUE_REQUIRED, 'Path to your database config file')
-			->addOption('mysql_root_password', null, InputOption::VALUE_REQUIRED, 'Root password')
-			->addOption('ssd', null, InputOption::VALUE_REQUIRED, 'Size of ssd storage')
-			->addOption('vcpu', null, InputOption::VALUE_REQUIRED, 'The number of virtual cpu cores available');
+		$this->setDescription('Create a new database')
+			->setHelp('https://www.lamp.io/api#/databases/databasesCreate')
+			->addOption('description', 'd', InputOption::VALUE_REQUIRED, 'Description of your database', '')
+			->addOption('memory', 'm', InputOption::VALUE_REQUIRED, 'Amount of virtual memory on your database, default 512Mi', '512Mi')
+			->addOption('organization_id', null, InputOption::VALUE_REQUIRED, 'Name of your organization', '')
+			->addOption('my_cnf', null, InputOption::VALUE_REQUIRED, 'Path to your database config file', '')
+			->addOption('mysql_root_password', null, InputOption::VALUE_REQUIRED, 'Root password', '')
+			->addOption('ssd', null, InputOption::VALUE_REQUIRED, 'Size of ssd storage, default 1Gi', '1Gi')
+			->addOption('vcpu', null, InputOption::VALUE_REQUIRED, 'The number of virtual cpu cores available, default 0.25', '0.25');
 	}
 
 	/**
@@ -53,17 +49,10 @@ class DatabaseUpdateCommand extends Command
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		parent::execute($input, $output);
-
-		$this->getRequestBody($input);
-
 		try {
 			$response = $this->httpHelper->getClient()->request(
-				'PATCH',
-				sprintf(
-					self::API_ENDPOINT,
-					$input->getArgument('database_id')
-
-				),
+				'POST',
+				self::API_ENDPOINT,
 				[
 					'headers' => $this->httpHelper->getHeaders(),
 					'body'    => $this->getRequestBody($input),
@@ -97,7 +86,7 @@ class DatabaseUpdateCommand extends Command
 			throw  new InvalidArgumentException('Path to mysql config not valid');
 		}
 
-		$body = [
+		return json_encode([
 			'data' => [
 				'attributes' => array_merge([
 					'description'         => $input->getOption('description'),
@@ -107,18 +96,9 @@ class DatabaseUpdateCommand extends Command
 					'ssd'                 => $input->getOption('ssd'),
 					'vcpu'                => (float)$input->getOption('vcpu'),
 				], !empty($input->getOption('organization_id')) ? ['organization_id' => $input->getOption('organization_id')] : []),
-				'id'         => $input->getArgument('database_id'),
 				'type'       => 'databases',
 			],
-		];
-
-		$body['data']['attributes'] = array_filter($body['data']['attributes'], function ($val, $key) {
-			if (!empty($val)) {
-				return [$key => $val];
-			}
-		}, ARRAY_FILTER_USE_BOTH);
-
-		return json_encode($body);
+		]);
 	}
 
 
