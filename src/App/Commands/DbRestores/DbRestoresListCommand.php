8ae50e9b57904accb9635941bdb7b32be034392c
Kirill <sooperkiras@gmail.com>
2019-07-28 01:16:42 +0300

    add db_restores:list command

---
+++ src/App/Commands/DbRestores/DbRestoresListCommand.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Console\App\Commands\DbRestores;
+
+use Art4\JsonApiClient\Exception\ValidationException;
+use Art4\JsonApiClient\Helper\Parser;
+use Art4\JsonApiClient\Serializer\ArraySerializer;
+use Art4\JsonApiClient\V1\Document;
+use Console\App\Commands\Command;
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DbRestoresListCommand extends Command
+{
+	const API_ENDPOINT = 'https://api.lamp.io/db_restores%s';
+
+	const OPTIONS_TO_QUERY_KEYS = [
+		'organization_id' => 'filter[organization_id]',
+	];
+	/**
+	 * @var string
+	 */
+	protected static $defaultName = 'db_restores:list';
+
+	/**
+	 *
+	 */
+	protected function configure()
+	{
+		parent::configure();
+		$this->setDescription('Return db restore jobs')
+			->setHelp('https://www.lamp.io/api#/db_restores/dbRestoresList')
+			->addOption('organization_id', 'o', InputOption::VALUE_REQUIRED, 'Comma-separated list of requested organization_ids. If omitted defaults to user\'s default organization');
+	}
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int|null|void
+	 * @throws Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+
+		try {
+			$response = $this->httpHelper->getClient()->request(
+				'GET',
+				sprintf(
+					self::API_ENDPOINT,
+					$this->httpHelper->optionsToQuery($input->getOptions(), self::OPTIONS_TO_QUERY_KEYS)
+				),
+				[
+					'headers' => $this->httpHelper->getHeaders(),
+				]
+			);
+			if (!empty($input->getOption('json'))) {
+				$output->writeln($response->getBody()->getContents());
+			} else {
+				/** @var Document $document */
+				$document = Parser::parseResponseString($response->getBody()->getContents());
+				$table = $this->getOutputAsTable($document, new Table($output));
+				$table->render();
+			}
+		} catch (GuzzleException $guzzleException) {
+			$output->writeln($guzzleException->getMessage());
+			exit(1);
+		} catch (ValidationException $e) {
+			$output->writeln($e->getMessage());
+			exit(1);
+		}
+	}
+
+	/**
+	 * @param Document $document
+	 * @param Table $table
+	 * @return Table
+	 */
+	protected function getOutputAsTable(Document $document, Table $table): Table
+	{
+		$table->setHeaderTitle('Database Restore Jobs');
+		$table->setStyle('box');
+		$table->setHeaders([
+			'Id', 'Database', 'Db backup', 'Complete', 'Created at', 'Organization Id', 'Status', 'Updated at',
+		]);
+		$serializer = new ArraySerializer(['recursive' => true]);
+		$serializedDocument = $serializer->serialize($document);
+		foreach ($serializedDocument['data'] as $key => $value) {
+			$table->addRow([
+				$value['id'],
+				$document->get('data.' . $key . '.attributes.target_database_id'),
+				$document->get('data.' . $key . '.attributes.db_backup_id'),
+				$document->get('data.' . $key . '.attributes.complete'),
+				$document->get('data.' . $key . '.attributes.created_at'),
+				$document->get('data.' . $key . '.attributes.organization_id'),
+				$document->get('data.' . $key . '.attributes.status'),
+				$document->get('data.' . $key . '.attributes.updated_at'),
+			]);
+			if ($key != count($serializedDocument['data']) - 1) {
+				$table->addRow(new TableSeparator());
+			}
+
+		}
+		return $table;
+	}
+}
