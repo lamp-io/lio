7c18cd8c26817a87e878705defda17fd608c01bf
Kirill <sooperkiras@gmail.com>
2019-07-28 01:28:11 +0300

    add db_restores:delete command

---
+++ src/App/Commands/DbRestores/DbRestoresDeleteCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Console\App\Commands\DbRestores;
+
+use Console\App\Commands\Command;
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DbRestoresDeleteCommand extends Command
+{
+	protected static $defaultName = 'db_restores:delete';
+
+	const API_ENDPOINT = 'https://api.lamp.io/db_restores/%s';
+
+	/**
+	 *
+	 */
+	protected function configure()
+	{
+		parent::configure();
+		$this->setDescription('Delete a db restore job')
+			->setHelp('https://www.lamp.io/api#/db_restores/dbRestoresDelete')
+			->addArgument('db_restore_id', InputArgument::REQUIRED, 'The ID of the db restore');
+	}
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int|void|null
+	 * @throws Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+		try {
+			$response = $this->httpHelper->getClient()->request(
+				'DELETE',
+				sprintf(
+					self::API_ENDPOINT,
+					$input->getArgument('db_restore_id')
+				),
+				[
+					'headers' => $this->httpHelper->getHeaders(),
+				]
+			);
+			if (!empty($input->getOption('json'))) {
+				$output->writeln($response->getBody()->getContents());
+			} else {
+				$output->writeln(
+					'<info>Db restore job deleted ' . $input->getArgument('db_restore_id') . '</info>'
+				);
+			}
+		} catch (GuzzleException $guzzleException) {
+			$output->writeln('<error>' . $guzzleException->getMessage() . '</error>');
+			die();
+		}
+	}
+}
