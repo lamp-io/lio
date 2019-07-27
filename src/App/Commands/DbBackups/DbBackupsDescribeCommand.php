49ff024acc2342c5ffc22701930459b76401391c
Kirill <sooperkiras@gmail.com>
2019-07-28 00:06:33 +0300

    add db_backups:describe command

---
+++ src/App/Commands/DbBackups/DbBackupsDescribeCommand.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Console\App\Commands\DbBackups;
+
+use Art4\JsonApiClient\Helper\Parser;
+use Art4\JsonApiClient\V1\Document;
+use Console\App\Commands\Command;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DbBackupsDescribeCommand extends Command
+{
+	protected static $defaultName = 'db_backups:describe';
+
+	const API_ENDPOINT = 'https://api.lamp.io/db_backups/%s';
+
+	/**
+	 *
+	 */
+	protected function configure()
+	{
+		parent::configure();
+		$this->setDescription('Return a db backup')
+			->setHelp('https://www.lamp.io/api#/db_backups/dbBackupsShow')
+			->addArgument('db_backup_id', InputArgument::REQUIRED, 'The ID of the db backup');
+	}
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int|void|null
+	 * @throws \Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+
+		try {
+			$response = $this->httpHelper->getClient()->request(
+				'GET',
+				sprintf(
+					self::API_ENDPOINT,
+					$input->getArgument('db_backup_id')
+				),
+				[
+					'headers' => $this->httpHelper->getHeaders(),
+				]
+			);
+			if (!empty($input->getOption('json'))) {
+				$output->writeln($response->getBody()->getContents());
+			} else {
+				/** @var Document $document */
+				$document = Parser::parseResponseString($response->getBody()->getContents());
+				$table = $this->getOutputAsTable($document, new Table($output));
+				$table->render();
+			}
+		} catch (GuzzleException $guzzleException) {
+			$output->writeln('<error>' . $guzzleException->getMessage() . '</error>');
+			exit(1);
+		}
+	}
+
+	/**
+	 * @param Document $document
+	 * @param Table $table
+	 * @return Table
+	 */
+	protected function getOutputAsTable(Document $document, Table $table): Table
+	{
+		$table->setHeaderTitle('Db Backup ' . $document->get('data.id'));
+		$table->setHeaders(['Db id', 'Complete', 'Created at', 'Organization id', 'Status', 'Updated at']);
+		$table->addRow([
+			$document->get('data.attributes.database_id'),
+			$document->get('data.attributes.complete'),
+			$document->get('data.attributes.created_at'),
+			$document->get('data.attributes.organization_id'),
+			$document->get('data.attributes.status'),
+			$document->get('data.attributes.updated_at'),
+		]);
+		return $table;
+	}
+}
