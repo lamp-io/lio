ef699f15e904260bc90e05a47065050124e3da89
KirillPerepelitsa <Sooperkiras@gmail.com>
2019-07-31 16:55:38 +0300

    add tokens:list command

---
+++ src/App/Commands/Tokens/TokensListCommand.php
@@ -0,0 +1,105 @@
+<?php
+
+
+namespace Console\App\Commands\Tokens;
+
+
+use Art4\JsonApiClient\Exception\ValidationException;
+use Art4\JsonApiClient\Helper\Parser;
+use Art4\JsonApiClient\Serializer\ArraySerializer;
+use Art4\JsonApiClient\V1\Document;
+use Console\App\Commands\Command;
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TokensListCommand extends Command
+{
+	const API_ENDPOINT = 'https://api.lamp.io/tokens';
+
+	/**
+	 * @var string
+	 */
+	protected static $defaultName = 'tokens:list';
+
+	/**
+	 *
+	 */
+	protected function configure()
+	{
+		parent::configure();
+		$this->setDescription('Returns all tokens for this user')
+			->setHelp('https://www.lamp.io/api#/tokens/tokensList');
+	}
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int|null|void
+	 * @throws Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+
+		try {
+			$response = $this->httpHelper->getClient()->request(
+				'GET',
+				self::API_ENDPOINT,
+				[
+					'headers' => $this->httpHelper->getHeaders(),
+				]
+			);
+			if (!empty($input->getOption('json'))) {
+				$output->writeln($response->getBody()->getContents());
+			} else {
+				/** @var Document $document */
+				$document = Parser::parseResponseString($response->getBody()->getContents());
+				$table = $this->getOutputAsTable($document, new Table($output));
+				$table->render();
+			}
+		} catch (GuzzleException $guzzleException) {
+			$output->writeln($guzzleException->getMessage());
+			exit(1);
+		} catch (ValidationException $e) {
+			$output->writeln($e->getMessage());
+			exit(1);
+		}
+	}
+
+	/**
+	 * @param Document $document
+	 * @param Table $table
+	 * @return Table
+	 */
+	protected function getOutputAsTable(Document $document, Table $table): Table
+	{
+		$table->setHeaderTitle('Tokens');
+		$table->setStyle('box');
+		$table->setHeaders([
+			'Id', 'Attributes',
+		]);
+		$serializer = new ArraySerializer(['recursive' => true]);
+		$serializedDocument = $serializer->serialize($document);
+		$sortedData = $this->sortData($serializedDocument['data'], 'updated_at');
+		$lastElement = end($sortedData);
+		foreach ($sortedData as $key => $data) {
+			$attributes = [];
+			foreach ($data['attributes'] as $attributeKey => $attribute) {
+				array_push($attributes, $attributeKey . ' : ' . $attribute);
+			}
+			$table->addRow([
+				$data['id'],
+				implode(PHP_EOL, $attributes),
+			]);
+
+			if ($lastElement != $data) {
+				$table->addRow(new TableSeparator());
+			}
+		}
+		return $table;
+	}
+}
