a0301263141892f66d2a0280bb6f8e8b00769174
KirillPerepelitsa <Sooperkiras@gmail.com>
2019-07-31 19:49:06 +0300

    update databases:update password handling

--- src/App/Helpers/PasswordHelper.php
+++ src/App/Helpers/PasswordHelper.php
@@ -2,7 +2,6 @@
 
 namespace Console\App\Helpers;
 
-use Exception;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 
@@ -20,20 +19,10 @@ class PasswordHelper
 		$question->setHiddenFallback(false);
 		$question->setHidden(true);
 		$question->setValidator(function ($value) use($output) {
-			$number = preg_match('/[0-9]|[\W]+/', $value);
-			$length = strlen($value) >= 8;
 			if (is_null($value)) {
 				$output->writeln('<error>Error: Refusing to set empty password</error>');
 				exit(1);
 			}
-			if ((!$length || !$number)) {
-				$exceptionMessage = [
-					'* Must be a minimum of 8 characters',
-					'* Must contain at least 1 number or 1 symbol',
-				];
-				throw new Exception(PHP_EOL . implode(PHP_EOL, $exceptionMessage));
-			}
-
 			return $value;
 		});
 		$question->setMaxAttempts(10);
