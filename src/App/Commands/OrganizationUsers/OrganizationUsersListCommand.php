8f027da559aa8ee540cb383e4bfa2dbb4cef3bd4
Kirill <sooperkiras@gmail.com>
2019-08-02 22:00:43 +0300

    add organization_users:list command

---
+++ src/App/Commands/OrganizationUsers/OrganizationUsersListCommand.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Console\App\Commands\OrganizationUsers;
+
+use Art4\JsonApiClient\Exception\ValidationException;
+use Art4\JsonApiClient\Helper\Parser;
+use Art4\JsonApiClient\Serializer\ArraySerializer;
+use Art4\JsonApiClient\V1\Document;
+use Console\App\Commands\Command;
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class OrganizationUsersListCommand extends Command
+{
+	const API_ENDPOINT = 'https://api.lamp.io/organization_users%s';
+
+	const OPTIONS_TO_QUERY_KEYS = [
+		'organization_id' => 'filter[organization_id]',
+	];
+
+	/**
+	 * @var string
+	 */
+	protected static $defaultName = 'organizations_users:list';
+
+	/**
+	 *
+	 */
+	protected function configure()
+	{
+		parent::configure();
+		$this->setDescription('Returns organization/user relationships.')
+			->setHelp('https://www.lamp.io/api#/organization_users/organizationUsersList')
+			->addOption('organization_id', null, InputOption::VALUE_REQUIRED, 'Comma-separated list of requested organization_ids. If omitted defaults to user\'s default organization');
+	}
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int|null|void
+	 * @throws Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+
+		try {
+			$response = $this->httpHelper->getClient()->request(
+				'GET',
+				sprintf(
+					self::API_ENDPOINT,
+					$this->httpHelper->optionsToQuery($input->getOptions(), self::OPTIONS_TO_QUERY_KEYS)
+				),
+				[
+					'headers' => $this->httpHelper->getHeaders(),
+				]
+			);
+			if (!empty($input->getOption('json'))) {
+				$output->writeln($response->getBody()->getContents());
+			} else {
+				/** @var Document $document */
+				$document = Parser::parseResponseString($response->getBody()->getContents());
+				$table = $this->getOutputAsTable($document, new Table($output));
+				$table->render();
+			}
+		} catch (GuzzleException $guzzleException) {
+			$output->writeln($guzzleException->getMessage());
+			exit(1);
+		} catch (ValidationException $e) {
+			$output->writeln($e->getMessage());
+			exit(1);
+		}
+	}
+
+	/**
+	 * @param Document $document
+	 * @param Table $table
+	 * @return Table
+	 */
+	protected function getOutputAsTable(Document $document, Table $table): Table
+	{
+		$table->setHeaderTitle('Organizations Users');
+		$table->setStyle('box');
+		$table->setHeaders([
+			'Id', 'Attributes',
+		]);
+		$serializer = new ArraySerializer(['recursive' => true]);
+		$serializedDocument = $serializer->serialize($document);
+		$sortedData = $this->sortData($serializedDocument['data'], 'updated_at');
+		$lastElement = end($sortedData);
+		foreach ($sortedData as $key => $data) {
+			$attributes = [];
+			foreach ($data['attributes'] as $attributeKey => $attribute) {
+				array_push($attributes, $attributeKey . ' : ' . $attribute);
+			}
+			$table->addRow([
+				$data['id'],
+				implode(PHP_EOL, $attributes),
+			]);
+
+			if ($lastElement != $data) {
+				$table->addRow(new TableSeparator());
+			}
+		}
+		return $table;
+	}
+}
