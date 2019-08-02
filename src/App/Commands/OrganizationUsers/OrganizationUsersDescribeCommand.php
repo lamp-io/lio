8753c70e3c1a06e97187f3fac31fafd0173f2c09
Kirill <sooperkiras@gmail.com>
2019-08-02 22:09:13 +0300

    add organization_users:describe command

---
+++ src/App/Commands/OrganizationUsers/OrganizationUsersDescribeCommand.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Console\App\Commands\OrganizationUsers;
+
+use Art4\JsonApiClient\Exception\ValidationException;
+use Art4\JsonApiClient\Helper\Parser;
+use Art4\JsonApiClient\Serializer\ArraySerializer;
+use Art4\JsonApiClient\V1\Document;
+use Console\App\Commands\Command;
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class OrganizationUsersDescribeCommand extends Command
+{
+	const API_ENDPOINT = 'https://api.lamp.io/organization_users/%s';
+
+	/**
+	 * @var string
+	 */
+	protected static $defaultName = 'organizations_users:describe';
+
+	/**
+	 *
+	 */
+	protected function configure()
+	{
+		parent::configure();
+		$this->setDescription('Returns a organization/user relationship')
+			->setHelp('https://www.lamp.io/api#/organization_users/organizationUsersShow')
+			->addArgument('organization_user_id', InputArgument::REQUIRED, 'The ID of the organization_user.');
+	}
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int|null|void
+	 * @throws Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+
+		try {
+			$response = $this->httpHelper->getClient()->request(
+				'GET',
+				sprintf(
+					self::API_ENDPOINT,
+					$input->getArgument('organization_user_id')
+				),
+				[
+					'headers' => $this->httpHelper->getHeaders(),
+				]
+			);
+			if (!empty($input->getOption('json'))) {
+				$output->writeln($response->getBody()->getContents());
+			} else {
+				/** @var Document $document */
+				$document = Parser::parseResponseString($response->getBody()->getContents());
+				$table = $this->getOutputAsTable($document, new Table($output));
+				$table->render();
+			}
+		} catch (GuzzleException $guzzleException) {
+			$output->writeln($guzzleException->getMessage());
+			exit(1);
+		} catch (ValidationException $e) {
+			$output->writeln($e->getMessage());
+			exit(1);
+		}
+	}
+
+	/**
+	 * @param Document $document
+	 * @param Table $table
+	 * @return Table
+	 */
+	protected function getOutputAsTable(Document $document, Table $table): Table
+	{
+		$table->setHeaderTitle('Organization User');
+		$table->setStyle('box');
+		$table->setHeaders([
+			'Id', 'Attributes',
+		]);
+		$serializer = new ArraySerializer(['recursive' => true]);
+		$serializedDocument = $serializer->serialize($document);
+		$attributes = [];
+		foreach ($serializedDocument['data']['attributes'] as $attributeKey => $attribute) {
+			array_push($attributes, $attributeKey . ' : ' . $attribute);
+		}
+		$table->addRow([
+			$serializedDocument['data']['id'],
+			implode(PHP_EOL, $attributes),
+		]);
+		return $table;
+	}
+}
