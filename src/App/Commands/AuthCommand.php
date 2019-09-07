a5d097f95135c5e9f16c9e7a3267de3a6085b3f1
Kirill <sooperkiras@gmail.com>
2019-09-07 16:13:38 +0300

    add --token option to auth command

--- src/App/Commands/AuthCommand.php
+++ src/App/Commands/AuthCommand.php
@@ -3,6 +3,7 @@
 namespace Console\App\Commands;
 
 use Console\App\Helpers\AuthHelper;
+use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,45 +13,53 @@ use Symfony\Component\Console\Question\Question;
 
 class AuthCommand extends Command
 {
-    const TOKEN_LENGTH = '52';
+	const TOKEN_LENGTH = '52';
 
-    const TOKEN_ATTEMPTS = '3';
+	const TOKEN_ATTEMPTS = '3';
 
-    protected static $defaultName = 'auth';
+	protected static $defaultName = 'auth';
 
-    /**
-     *
-     */
-    protected function configure()
-    {
-        $this->setDescription('Set auth token')
-            ->setHelp('Get your token at https://www.lamp.io/ on settings page')
-            ->addOption('update_token', '-u', InputOption::VALUE_NONE, 'Update existing token');
-    }
+	/**
+	 *
+	 */
+	protected function configure()
+	{
+		$this->setDescription('Set auth token')
+			->setHelp('Get your token at https://www.lamp.io/ on settings page')
+			->addOption('update_token', 'u', InputOption::VALUE_NONE, 'Update existing token')
+			->addOption('token', 't', InputOption::VALUE_REQUIRED, 'Set auth token, in noninteractive mode');
+	}
 
-    /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        /** @var QuestionHelper $questionHelper */
-        $questionHelper = $this->getHelper('question');
-        if (AuthHelper::isTokenExist() && empty($input->getOption('update_token'))) {
-            $output->writeln(' <info>Token already exist, if you want to update it, please add -u/--update_token option </info>');
-        } else {
-            $question = new Question('Please write your auth token' . PHP_EOL);
-            $question->setValidator(function ($answer) {
-                if (empty($answer) || strlen($answer) < self::TOKEN_LENGTH) {
-                    throw new \RuntimeException(
-                        'Invalid Token. Get your token at https://www.lamp.io/ on settings page'
-                    );
-                }
-                return $answer;
-            });
-            $question->setMaxAttempts(self::TOKEN_ATTEMPTS);
-            AuthHelper::saveToken($questionHelper->ask($input, $output, $question));
-            $output->writeln('<info>Token saved</info>');
-        }
-    }
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return mixed
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		if (!empty($input->getOption('token'))) {
+			$token = $input->getOption('token');
+		} else {
+			/** @var QuestionHelper $questionHelper */
+			$questionHelper = $this->getHelper('question');
+			if (AuthHelper::isTokenExist() && empty($input->getOption('update_token'))) {
+				$output->writeln(' <info>Token already exist, if you want to update it, please add [-u][--update_token] option </info>');
+				return 1;
+			} else {
+				$question = new Question('Please write your auth token' . PHP_EOL);
+				$question->setValidator(function ($answer) {
+					if (empty($answer) || strlen($answer) < self::TOKEN_LENGTH) {
+						throw new RuntimeException(
+							'Invalid Token. Get your token at https://www.lamp.io/ on settings page'
+						);
+					}
+					return $answer;
+				});
+				$question->setMaxAttempts(self::TOKEN_ATTEMPTS);
+				$token = $questionHelper->ask($input, $output, $question);
+			}
+		}
+		AuthHelper::saveToken($token);
+		$output->writeln('<info>Token saved</info>');
+	}
 }
