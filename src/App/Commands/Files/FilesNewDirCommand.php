b8344e214ac5f22594a7049725723842cae66e5d
KirillPerepelitsa <Sooperkiras@gmail.com>
2019-09-27 18:14:11 +0300

    add file:new:dir command

--- src/App/Commands/Files/FilesNewDirCommand.php
+++ src/App/Commands/Files/FilesNewDirCommand.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class FilesNewDirCommand extends Command
 {
-	const API_ENDPOINT = 'https://api.lamp.io/apps/%s/files/%s';
+	const API_ENDPOINT = 'https://api.lamp.io/apps/%s/files/';
 
 	protected static $defaultName = 'files:new:dir';
 
@@ -24,8 +24,8 @@ class FilesNewDirCommand extends Command
 	protected function configure()
 	{
 		parent::configure();
-		$this->setDescription('Create directory on your app')
-			->setHelp('Create directory, api reference' . PHP_EOL . 'https://www.lamp.io/api#/files/filesCreate')
+		$this->setDescription('Create a directory on your app')
+			->setHelp('Create a directory, api reference' . PHP_EOL . 'https://www.lamp.io/api#/files/filesCreate')
 			->addArgument('app_id', InputArgument::REQUIRED, 'The ID of the app')
 			->addArgument('file_id', InputArgument::REQUIRED, 'File ID of directory to create');
 	}
@@ -40,31 +40,27 @@ class FilesNewDirCommand extends Command
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		parent::execute($input, $output);
-
-		$progressBar = self::getProgressBar('Deleting ' . $input->getArgument('file_id'), $output);
+		$progressBar = self::getProgressBar('Creating dir ' . $input->getArgument('file_id'), $output);
 		try {
-			$this->httpHelper->getClient()->request(
+			$response = $this->httpHelper->getClient()->request(
 				'POST',
 				sprintf(
 					self::API_ENDPOINT,
-					$input->getArgument('app_id'),
-					trim($input->getArgument('file_id'), '/')
-
+					$input->getArgument('app_id')
 				),
 				[
-					'headers'  => [
-						'Accept'        => 'application/json',
-						'Authorization' => $this->httpHelper->getHeader('Authorization'),
-					],
-					'body'     => $this->getRequestBody(),
+					'headers'  => $this->httpHelper->getHeaders(),
+					'body'     => $this->getRequestBody(trim($input->getArgument('file_id'), '/')),
 					'progress' => function () use ($progressBar) {
 						$progressBar->advance();
 					},
 				]);
 			$progressBar->finish();
 			$output->write(PHP_EOL);
-			if (empty($input->getOption('json'))) {
-				$output->writeln('<info>Success, ' . $input->getArgument('file_id') . ' has been deleted</info>');
+			if (!empty($input->getOption('json'))) {
+				$output->writeln($response->getBody()->getContents());
+			} else {
+				$output->writeln('<info>Success, directory ' . $input->getArgument('file_id') . ' has been created</info>');
 			}
 		} catch (BadResponseException $badResponseException) {
 			$output->writeln('<error>' . $badResponseException->getResponse()->getBody()->getContents() . '</error>');
@@ -77,10 +73,9 @@ class FilesNewDirCommand extends Command
 		return json_encode([
 			'data' => [
 				'type'       => 'files',
-				'id'         => trim($fileId),
+				'id'         => $fileId,
 				'attributes' => [
-					'contents' => '',
-					'is_dir'   => true,
+					'is_dir' => true,
 				],
 			],
 		]);
