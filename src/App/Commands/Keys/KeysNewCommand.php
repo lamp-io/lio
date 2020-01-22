abbe049f15f756c86180c25feaccac96a5e2c475
KirillPerepelitsa <Sooperkiras@gmail.com>
2020-01-22 12:16:31 +0200

    organization id as otpion, fix request body for case when empty attributes

--- src/App/Commands/Keys/KeysNewCommand.php
+++ src/App/Commands/Keys/KeysNewCommand.php
@@ -6,7 +6,6 @@ use Art4\JsonApiClient\Helper\Parser;
 use Art4\JsonApiClient\V1\Document;
 use Lio\App\AbstractCommands\AbstractNewCommand;
 use Psr\Http\Message\ResponseInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -25,7 +24,7 @@ class KeysNewCommand extends AbstractNewCommand
 		parent::configure();
 		$this->setDescription('Creates a new key')
 			->setHelp('Create key, api reference' . PHP_EOL . 'https://www.lamp.io/api#/keys/keysCreate')
-			->addArgument('organization_id', InputArgument::REQUIRED, '')
+			->addOption('organization_id', 'o', InputOption::VALUE_REQUIRED, 'The organization this key belongs to')
 			->addOption('description', 'd', InputOption::VALUE_REQUIRED, 'An immutable description for this key')
 			->setApiEndpoint(self::API_ENDPOINT);
 	}
@@ -49,15 +48,19 @@ class KeysNewCommand extends AbstractNewCommand
 	 */
 	protected function getRequestBody(InputInterface $input): string
 	{
+		$attributes = [
+			'description'     => $input->getOption('description') ?? '',
+			'organization_id' => $input->getOption('organization_id') ?? '',
+		];
+
 		return json_encode([
 			'data' => [
-				'attributes' => [
-					'description'     => $input->getOption('description') ?? '',
-					'organization_id' => $input->getArgument('organization_id'),
-				],
+				'attributes' => array_filter($attributes, function ($attribute) {
+					return !empty($attribute);
+				}),
 				'type'       => 'keys',
 			],
-		]);
+		], JSON_FORCE_OBJECT);
 	}
 
 }
